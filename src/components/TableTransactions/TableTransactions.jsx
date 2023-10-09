a20e3cb7384226f06f84d462048a413a8f02e508
rafawessling <rafaoening@gmail.com>
2023-10-09 11:30:21 +0200

    feat: Added responsive layout to Home page

--- src/components/TableTransactions/TableTransactions.jsx
+++ src/components/TableTransactions/TableTransactions.jsx
@@ -30,45 +30,47 @@ function TableTransactions({
     };
 
     return (
-        <table className="main-table">
-            <thead className="head-main-table">
-                <tr>
-                    <th className="col1 date-head">
-                        Date{' '}
-                        <img
-                            src={orderDate === 'oldFirst' ? SortDatesOld : SortDatesNew}
-                            alt="Blue triangle"
-                            onClick={() => handleSortDates()}
+        <section className="table-container">
+            <table className="main-table">
+                <thead className="head-main-table">
+                    <tr>
+                        <th className="col1 date-head">
+                            Date{' '}
+                            <img
+                                src={orderDate === 'oldFirst' ? SortDatesOld : SortDatesNew}
+                                alt="Blue triangle"
+                                onClick={() => handleSortDates()}
+                            />
+                        </th>
+                        <th className="col2">Day of the week</th>
+                        <th className="col3">Description</th>
+                        <th className="col4">Category</th>
+                        <th className="col5">Amount</th>
+                        <th className="col6">&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody className={showFilter ? 'container-body-table smaller-table' : 'container-body-table'}>
+                    {!isFiltered ? (
+                        <Transactions
+                            transactions={transactions}
+                            setTransactions={setTransactions}
+                            setEditTransaction={setEditTransaction}
+                            setCurrentTransaction={setCurrentTransaction}
+                            isFiltered={isFiltered}
+                            filteredTransactions={filteredTransactions}
                         />
-                    </th>
-                    <th className="col2">Day of the week</th>
-                    <th className="col3">Description</th>
-                    <th className="col4">Category</th>
-                    <th className="col5">Amount</th>
-                    <th className="col6">&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody className={showFilter ? 'container-body-table smaller-table' : 'container-body-table'}>
-                {!isFiltered ? (
-                    <Transactions
-                        transactions={transactions}
-                        setTransactions={setTransactions}
-                        setEditTransaction={setEditTransaction}
-                        setCurrentTransaction={setCurrentTransaction}
-                        isFiltered={isFiltered}
-                        filteredTransactions={filteredTransactions}
-                    />
-                ) : (
-                    <Transactions
-                        transactions={filteredTransactions}
-                        setTransactions={setTransactions}
-                        setEditTransaction={setEditTransaction}
-                        setCurrentTransaction={setCurrentTransaction}
-                        isFiltered={isFiltered}
-                    />
-                )}
-            </tbody>
-        </table>
+                    ) : (
+                        <Transactions
+                            transactions={filteredTransactions}
+                            setTransactions={setTransactions}
+                            setEditTransaction={setEditTransaction}
+                            setCurrentTransaction={setCurrentTransaction}
+                            isFiltered={isFiltered}
+                        />
+                    )}
+                </tbody>
+            </table>
+        </section>
     );
 }
 
